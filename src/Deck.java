8d59d87d6bc627bec0e8c5a703632683cbfceea8
devon <devon.geertsen@gmail.com>
2022-12-13 19:38:32 -0700

    QoL Updates

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Deck {
+		
+    private List<Card> cards = new ArrayList<Card>();
+    private List<String> suits = Arrays.asList("Diamonds", "Hearts", "Clubs", "Spades");
+        
+    //Constructor
+    public Deck() {
+    	for (int i=2;i<15;i++) {
+    		for(String suit:suits) {
+    			cards.add(new Card(i,suit));
+    		}
+    	}
+    	
+    	
+    }
+    
+    
+    
+    
+    public void shuffle() {
+    	Collections.shuffle(cards);
+    }
+    
+    public Card draw() {
+    	return cards.remove(0);
+    }
+    
+    
+    public void describe() {
+    	for(Card card:cards) {
+    		System.out.println(card.describe());
+    	}
+    }
+    
 
+
+
+    
+    
+	
+	
 }
